b2c0f2c5d9274eaaa6ac267e632fc75e2b10757e
Erik Lelis <erikslelis@hotmail.com>
2022-04-05 23:58:01 -0300

    Criando  publisher

--- src/main/java/guru/springframework/spring5webapp/domain/Book.java
+++ src/main/java/guru/springframework/spring5webapp/domain/Book.java
@@ -1,5 +1,6 @@
 package guru.springframework.spring5webapp.domain;
 
+import java.util.HashSet;
 import java.util.Set;
 
 import javax.persistence.Entity;
@@ -9,6 +10,8 @@ import javax.persistence.Id;
 import javax.persistence.JoinColumn;
 import javax.persistence.JoinTable;
 import javax.persistence.ManyToMany;
+import javax.persistence.ManyToOne;
+import javax.persistence.OneToMany;
 
 @Entity
 public class Book {
@@ -19,16 +22,20 @@ public class Book {
 	private String title;
 	private String isbn;
 	@ManyToMany
+	
 	@JoinTable(name = "author_book", joinColumns = @JoinColumn(name = "book_id"),
 				inverseJoinColumns = @JoinColumn(name = "author_id"))
-	private Set<Author> authors;
+	private Set<Author> authors = new HashSet<>();
+	
+	@ManyToOne
+	@JoinColumn(name="book_id")
+	private Publisher publisher;
 	
 	public Book() {}
 	
-	public Book(String title, String isbn, Set<Author> authors) {
+	public Book(String title, String isbn) {
 		this.title = title;
 		this.isbn = isbn;
-		this.authors = authors;
 	}
 
 	public String getTitle() {
@@ -59,7 +66,13 @@ public class Book {
 		this.id = id;
 	}
 	
-	
+	public Publisher getPublisher() {
+		return publisher;
+	}
+
+	public void setPublisher(Publisher publisher) {
+		this.publisher = publisher;
+	}
 
 	@Override
 	public String toString() {
